3f787a42ddf4b425ddd78b7124526f1b853acf59
nastassia <anastasiyamihalenko@gmail.com>
2020-12-07 22:21:46 +0300

    hw3

---
+++ script.js
@@ -0,0 +1,96 @@
+// The task 1
+
+console.log('The task 1')
+for (let i1 = 1; i1 <=50; i1++){
+	console.log(i1);
+}
+
+for (let I = 35; I <= 35 && I >= 8; I--){
+	console.log(I);
+}
+
+// The task 2
+
+console.log('The task 2')
+let i2 = 89;
+while (i2 <= 89 && i2 >= 11){
+	console.log(i2--);
+}
+
+// The task 3
+
+
+console.log('The task 3')
+let a3 = 100; //
+sum = 0;
+for (let i3 =1; i3 <= a3; i3++){
+	sum += i3;
+}
+
+console.log(sum);  
+
+// The task 4
+
+console.log('The task 4')
+let a4 = 5;
+let sum4 = 0;
+for (let i4 =1; i4 <= a4; i4++){
+	sum4 = 0;
+	for(let j4 = 1; j4 <=i4; j4++){
+		sum4 += j4;
+	}
+
+	console.log('sum4' + i4 + '=' + sum4);
+}
+
+// The task 5
+
+console.log('The task 5')
+console.log('for')
+
+for(let i5 = 8; i5 <= 56; i5++){
+	if (i5 % 2 == 0)  
+	console.log(i5);
+}
+
+console.log('while')
+let a5 = 8;
+while (a5 <= 56) {
+	a5++;
+	if (a5 % 2 == 0)
+	console.log(a5);
+}
+
+
+
+// The task 6
+
+console.log('The task 6')
+
+let i = 1;
+let o = 1
+for( let i = 1; i <=9; i++){
+	for( let o = 1; o<=10; o++)
+console.log (i + '*'  + o + '=' + (i * o));		
+}
+
+
+
+// The task 7
+
+console.log('The task 7');
+
+for (var n = 1000, num = 0; n > 50; n/=2){
+	num++;
+}
+console.log(n);
+console.log(num);
+
+// The task 8
+
+
+
+
+
+
+
